7123b5b1d6019ab6c0c07f62b480d6a1219778c3
unknown <cnsituma@gmail.com>
2025-06-27 16:54:40 +0300

    Initial Next.js project setup with TypeScript and layout structure

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/context/(.*)$': '<rootDir>/src/context/$1',
+  },
+  testEnvironment: 'jsdom',
+});
